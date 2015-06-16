4d51bebf848b234b898ad2b29cff76f69a896766
David Thaler <davidthaler@gmail.com>
2015-06-15 20:30:45 -0700

    added runOne.R

---
+++ runOne.R
@@ -0,0 +1,13 @@
+source('util.R')
+source('grouped.forecast.R')
+source('postprocess.R')
+
+train <- raw.train()
+test <- raw.test()
+
+# This is the best single model from the competition.
+pred <- grouped.forecast(train, test, 'stlf.svd', model.type='ets', n.comp=12)
+pred <- postprocess(train, pred, shift=2.5)
+snum <- write.submission(pred)
+print(paste('Wrote submission', snum, ' to ../submissions.'))
+
